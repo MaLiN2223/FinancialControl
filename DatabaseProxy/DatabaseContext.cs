253788fa3e5ed7968bdd1af0a6208731afb2fdd3
Maliniak <szyszka45@gmail.com>
2017-09-16 02:18:46 +0100

    database connection

---
+++ DatabaseProxy/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+
+namespace DatabaseProxy
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("name=FCdbConnectionString") { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Color Color { get; set; }
+    }
+
+    public class Color
+    {
+        public int Id { get; set; }
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+    }
+}
