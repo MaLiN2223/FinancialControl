544dfa27a1e4b65b900445b7039b6f7febf6dfcb
Maliniak <szyszka45@gmail.com>
2017-09-24 16:42:35 +0100

    move to file

--- FinancialControl.Service.UnitTests/FinancialControlServiceTests.cs
+++ FinancialControl.Service.UnitTests/FinancialControlServiceTests.cs
@@ -12,7 +12,7 @@ using Product = FinancialControl.Repositories.Dto.Product;
 
 namespace FinancialControl.Service.UnitTests
 {
-    public class FinancialControlServiceTests
+    public partial class FinancialControlServiceTests
     {
         public FinancialControlServiceTests()
         {
@@ -37,7 +37,6 @@ namespace FinancialControl.Service.UnitTests
             var actualCategories = _service.Get(new AllCategoriesRequest());
 
             //ASSERT
-
             Assert.Equal(expectedCategories, actualCategories);
         }
 
@@ -90,38 +89,6 @@ namespace FinancialControl.Service.UnitTests
             _proxy.Received(1)
                 .AddReceipt(Arg.Is<Receipt>(x =>
                     expectedProducts.SequenceEqual(x.Products, new ProductEqualityComparer())));
-        }
-        public class ProductEqualityComparer : IEqualityComparer<Product>
-        {
-            public bool Equals(Product x, Product y)
-            {
-                if (ReferenceEquals(x, y))
-                    return true;
-                if (x == null || y == null)
-                    return false;
-                return
-                    x.Category.Title.Equals(y.Category.Title) &
-                    x.Name.Equals(y.Name) &
-                    x.Price.Equals(y.Price) &
-                    x.Volume.Equals(y.Volume);
-
-            }
-
-            public int GetHashCode(Product obj)
-            {
-                // Stores the result.
-                unchecked // Overflow is fine, just wrap
-                {
-                    int hash = 17;
-                    // Suitable nullity checks etc, of course :)
-                    hash = hash * 23 + obj.Category.Title.GetHashCode();
-                    hash = hash * 23 + obj.Name.GetHashCode();
-                    hash = hash * 23 + obj.Price.GetHashCode();
-                    hash = hash * 23 + obj.Volume.GetHashCode();
-                    return hash;
-                }
-
-            }
         }
         [Fact]
         public void Post_CreateCategoryRequest_CalledProxyAddCategory()
---
+++ FinancialControl.Service.UnitTests/ProductEqualityComparer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Product = FinancialControl.Repositories.Dto.Product;
+
+namespace FinancialControl.Service.UnitTests
+{
+    public partial class FinancialControlServiceTests
+    {
+        public class ProductEqualityComparer : IEqualityComparer<Product>
+        {
+            public bool Equals(Product x, Product y)
+            {
+                if (ReferenceEquals(x, y))
+                    return true;
+                if (x == null || y == null)
+                    return false;
+                return
+                    x.Category.Title.Equals(y.Category.Title) &
+                    x.Name.Equals(y.Name) &
+                    x.Price.Equals(y.Price) &
+                    x.Volume.Equals(y.Volume);
+
+            }
+
+            public int GetHashCode(Product obj)
+            {
+                // Stores the result.
+                unchecked // Overflow is fine, just wrap
+                {
+                    int hash = 17;
+                    // Suitable nullity checks etc, of course :)
+                    hash = hash * 23 + obj.Category.Title.GetHashCode();
+                    hash = hash * 23 + obj.Name.GetHashCode();
+                    hash = hash * 23 + obj.Price.GetHashCode();
+                    hash = hash * 23 + obj.Volume.GetHashCode();
+                    return hash;
+                }
+
+            }
+        }
+    }
+}
